d1712583cf523bc74af070f224df5b8ae12b2e0a
Vincent Blouin <vincent.blouin@gmail.com>
2022-04-11 14:27:10 -0400

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,113 @@
 <template>
-  <v-app>
-    <v-app-bar
-        app
-        color="transparent"
-        dark
-        flat
-    >
-      <div class="d-flex align-center">
-        <!--        <v-img-->
-        <!--          alt="Vuetify Logo"-->
-        <!--          class="shrink mr-2"-->
-        <!--          contain-->
-        <!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
-        <!--          transition="scale-transition"-->
-        <!--          width="40"-->
-        <!--        />-->
-
-        <!--        <v-img-->
-        <!--          alt="Vuetify Name"-->
-        <!--          class="shrink mt-1 hidden-sm-and-down"-->
-        <!--          contain-->
-        <!--          min-width="100"-->
-        <!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
-        <!--          width="100"-->
-        <!--        />-->
-        <router-link to="/" class="text-decoration-none">
-          <h3 class="black--text">
-            <span>Ferme Coopérative Les Potagers Partagés</span>
-          </h3>
-        </router-link>
-      </div>
-
-      <v-spacer></v-spacer>
-      <v-btn to="/ouverture" text class="black--text" v-if="$vuetify.breakpoint.mdAndUp">Heures d'ouverture</v-btn>
-      <v-btn href="https://potagerspartages.ca/compte-prepaye-2022" text class="black--text" v-if="$vuetify.breakpoint.mdAndUp">Compte prépayé</v-btn>
-      <v-btn to="/" text class="black--text" v-if="$vuetify.breakpoint.mdAndUp">À propos</v-btn>
-      <v-btn to="/contact" text class="black--text" v-if="$vuetify.breakpoint.mdAndUp">Contactez-nous</v-btn>
-<!--      <v-btn to="/offre-emploi" text class="black&#45;&#45;text" v-if="$vuetify.breakpoint.mdAndUp">Offre d'emploi</v-btn>-->
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="black--text"
-                          v-if="$vuetify.breakpoint.smAndDown"></v-app-bar-nav-icon>
-      <v-navigation-drawer
-          clipped
-          v-model="drawer"
-          enable-resize-watcher
-          fixed
+  <div id="app">
+    <v-app>
+      <v-app-bar
           app
-          dense
-          hide-overlay
-          light
+          color="transparent"
+          dark
+          flat
       >
-        <v-list>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>
-                <v-btn to="/ouverture" text class="black--text">Heures d'ouverture</v-btn>
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>
-                <v-btn href="https://potagerspartages.ca/compte-prepaye-2022" text class="black--text">Compte prépayé</v-btn>
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>
-                <v-btn to="/" text class="black--text">À propos</v-btn>
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>
-                <v-btn to="/contact" text class="black--text">Contactez-nous</v-btn>
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <!--          <v-list-item>-->
-          <!--            <v-list-item-content>-->
-          <!--              <v-list-item-title>-->
-          <!--                <v-btn to="/offre-emploi" text class="black&#45;&#45;text">Offre d'emploi</v-btn>-->
-          <!--              </v-list-item-title>-->
-          <!--            </v-list-item-content>-->
-          <!--          </v-list-item>-->
-        </v-list>
-      </v-navigation-drawer>
-    </v-app-bar>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+        <div class="d-flex align-center">
+          <!--        <v-img-->
+          <!--          alt="Vuetify Logo"-->
+          <!--          class="shrink mr-2"-->
+          <!--          contain-->
+          <!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"-->
+          <!--          transition="scale-transition"-->
+          <!--          width="40"-->
+          <!--        />-->
+
+          <!--        <v-img-->
+          <!--          alt="Vuetify Name"-->
+          <!--          class="shrink mt-1 hidden-sm-and-down"-->
+          <!--          contain-->
+          <!--          min-width="100"-->
+          <!--          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"-->
+          <!--          width="100"-->
+          <!--        />-->
+          <router-link to="/" class="text-decoration-none">
+            <h3 class="black--text">
+              <span>Ferme Coopérative Les Potagers Partagés</span>
+            </h3>
+          </router-link>
+        </div>
+
+        <v-spacer></v-spacer>
+        <v-btn to="/ouverture" text class="black--text" v-if="$vuetify.breakpoint.mdAndUp">Heures d'ouverture</v-btn>
+        <v-btn href="https://potagerspartages.ca/compte-prepaye-2022" text class="black--text" v-if="$vuetify.breakpoint.mdAndUp">Compte prépayé</v-btn>
+        <v-btn to="/" text class="black--text" v-if="$vuetify.breakpoint.mdAndUp">À propos</v-btn>
+        <v-btn to="/contact" text class="black--text" v-if="$vuetify.breakpoint.mdAndUp">Contactez-nous</v-btn>
+        <!--      <v-btn to="/offre-emploi" text class="black&#45;&#45;text" v-if="$vuetify.breakpoint.mdAndUp">Offre d'emploi</v-btn>-->
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="black--text"
+                            v-if="$vuetify.breakpoint.smAndDown"></v-app-bar-nav-icon>
+        <v-navigation-drawer
+            clipped
+            v-model="drawer"
+            enable-resize-watcher
+            fixed
+            app
+            dense
+            hide-overlay
+            light
+        >
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-btn to="/ouverture" text class="black--text">Heures d'ouverture</v-btn>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-btn href="https://potagerspartages.ca/compte-prepaye-2022" text class="black--text">Compte prépayé</v-btn>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-btn to="/" text class="black--text">À propos</v-btn>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-btn to="/contact" text class="black--text">Contactez-nous</v-btn>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <!--          <v-list-item>-->
+            <!--            <v-list-item-content>-->
+            <!--              <v-list-item-title>-->
+            <!--                <v-btn to="/offre-emploi" text class="black&#45;&#45;text">Offre d'emploi</v-btn>-->
+            <!--              </v-list-item-title>-->
+            <!--            </v-list-item-content>-->
+            <!--          </v-list-item>-->
+          </v-list>
+        </v-navigation-drawer>
+      </v-app-bar>
+      <v-main>
+        <router-view/>
+      </v-main>
+    </v-app>
+    <v-footer
+        fixed
+        class="font-weight-medium"
+        dark
+    >
+      <v-col
+          class="vh-center body-1"
+          cols="12"
+      >
+        <strong class="body-1" style="letter-spacing: 3px;font-size:18px;">
+          Ferme à Paspébiac
+        </strong>
+      </v-col>
+    </v-footer>
+  </div>
 </template>
 
 <script>
